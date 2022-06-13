a45d24ef5064d32ecd862cf0ef12edc2f2979b1a
olegbar02 <olegbar02@icloud.com>
2022-06-13 21:25:32 +0300

    4th

--- pages/Telegram_bot_✈.py
+++ pages/Telegram_bot_✈.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown('''# Найди свой УИК
+st.markdown(""" # Найди свой УИК
             
             С помощью скрипта, который я написал, для поиска УИКа по адресу я решил сделать [бот в телеге]
-            ('https://t.me/sovbaker_bot')''')
+            ('https://t.me/sovbaker_bot'"""
