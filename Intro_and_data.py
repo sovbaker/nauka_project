45926603336e3c403e19777e32b5372c7c86978f
olegbar02 <olegbar02@icloud.com>
2022-06-13 03:07:06 +0300

    4th

--- Intro_and_data.py
+++ Intro_and_data.py
@@ -19,13 +19,17 @@ import requests
 
 st.set_page_config(layout="wide")
 with st.echo(code_location='below'):
+
     """
-    *>Ты есть то, что ты ешь*
-    Это цитата какого-то умного древнего грека, которая к мне кажется хорошо описывает мой проект
+    # Еда и выборы
+    
+    >Ты есть то, что ты ешь
+    
+    Это цитата какого-то умного древнего грека, которая как мне кажется, хорошо описывает мой проект
     В прошлый раз я делал анализ данных слива Яндекс Еды. В этот раз мой проект будет основывааться на сливе данных
     Деливери клаб. Я попытаюсь найти связя между тем, сколько люди тратят на еду и тем как они голосуют на выборах
     
-    ### Часть 1: Сбор и хранение данных
+    ### Сбор данных
     В файлах проекта вы найдете ноут из jupiter, где я собирал данные. Чтоб вы несильно утруждались читая его здесь я
     вкратце опишу, что происходило с данными.
     
@@ -73,16 +77,16 @@ with st.echo(code_location='below'):
                             .rename(columns={0:'bulding_num', 1:'suffix', 2:'suffix_num'}))
                             , left_index=True, right_index=True))
     ```
-    ### Неоптимальный вариант выполнения этой задачи с помощью web scrapping
+    ##### Неоптимальный вариант выполнения этой задачи с помощью web scrapping
     ```python
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get('http://www.cikrf.ru/digital-services/naydi-svoy-izbiratelnyy-uchastok/')
-        from selenium.webdriver.common.keys import Keys
-        from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, StaleElementReferenceException
-        from time import sleep, time
+    driver.get('http://www.cikrf.ru/digital-services/naydi-svoy-izbiratelnyy-uchastok/')
+    from selenium.webdriver.common.keys import Keys
+    from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, StaleElementReferenceException
+    from time import sleep, time
         
-        tnow=time()
-        for idx, row in df_for_address_parse.iterrows():
+    tnow=time()
+    for idx, row in df_for_address_parse.iterrows():
         city='Город Москва, '
         street=str(row.delivery2_address_street)+', '
         if len(row.suffix)!=1:
@@ -208,10 +212,134 @@ with st.echo(code_location='below'):
         print(idx, time()-tnow)
     ```
     
-    Почему неоптимальный? Потому что он парсит один адрес примерно 5 секунд
+    Почему неоптимальный? Потому что он парсит один адрес примерно 5 секунд, чтоб спарсить УИКи на весь датасет
+    придется ждать 15 дней
+    
     Делает он это примерно так:
+    """
+    ##видосик
+    """
+    ##### Оптимальный вариант
+    Когда мы пишем в ЦИК адрес он обращается к своему api и делает запрос, чтобы получить участок, однако
+    к сожалению получилось так, что через него не получается достать уик
+    , так как там стоит защита от ддос атак и он не дает подключиться модулю request и банит по ip
+    
+    ##### Не очень оптимальный, но хотя бы рабочий вариант
+    В итоге пришлось делать так. Через парсинг сайта мосгоризберкома, который не банит модуль requests
+    ```python
+    # Пошаманили с помощью регулярных выражений с улицами
+    ulitsy =df_for_address_parse.delivery2_address_street.str.lower()
+    .str.extract(r'(\w?\w?\w?\w?\w?\w?\w?
+    \w?\w?\w?\w?\w?\w?\w?\w?\w?\w?)
+    (\s?)(\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?
+    \w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?
+    \w?\w?)(\s?)(\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?
+    \w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?)(\s?)
+    (\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?\w?)')
+    
+    ulitsy[[0,2]]=ulitsy[[0,2]].mask((ulitsy[2]=='улица')|(ulitsy[2]=='проспект')|(ulitsy[2]=='шоссе')
+    |(ulitsy[2]=='проезд')|(ulitsy[2]=='пр-д')
+    |(ulitsy[2]=='пр-кт')|(ulitsy[2]=='ул')|(ulitsy[2]=='переулок'), ulitsy[[2,0]].values)
+    ulitsy.rename(columns={0:'type', 1:'space', 2:'nazv', 3:'add_name', 4:'add_name_2', 5:'add_name_3', 6:'add_name_4'}
+    , inplace=True)
+    df_for_address_parse=df_for_address_parse.merge(ulitsy, how='left', left_index=True, right_index=True)
+    
+    # Скрипт, который парсит сайт Мосгоризберкома
+    from time import sleep, time 
+    from requests.exceptions import RequestException
+    import requests
+    from bs4 import BeautifulSoup
+    tnow=time()
+    null_cnt=0
+    all_cnt=0
+    
+    for idx, row in df_for_address_parse
+        [(df_for_address_parse['UIK']=='null')&(df_for_address_parse['parse_flg']!=1)][7000:].iterrows():
+        try:
+            if row.str.contains(r'[\bулица\b]', regex=True).any():
+                street=str(row.nazv)+row.add_name+str(row.add_name_2).strip(' ')+str(row.add_name_3)+row.add_name_4
+            else:
+                street=row.type+row.space+row.nazv+row.add_name+row.add_name_2+row.add_name_3+row.add_name_4
+        
+            if len(street)<3:
+                street=row.delivery2_address_street
+            if len(row.suffix)!=1:
+                building=str(row.bulding_num)
+            elif row.suffix=='к':
+                building=str(row.bulding_num)+' '+'корп'+'. '+str(row.suffix_num)
+            else:
+                building=str(row.bulding_num)+' '+str(row.suffix)+'. '+str(row.suffix_num)
+            flat=', '+'кв. ' + str(row.delivery2_address_flat_number)
+            full_address=street+' '+building
+            q={'name':street}
+        
+            r=requests.get('http://mosgorizbirkom.ru/precinct/address/byname/', params=q, timeout=0.4)
+            r.encoding = 'utf-8'               
+            soup=BeautifulSoup(r.text)
+            if soup.find('a')==None:
+                result='null'
+            else:
+                a = soup.find('a')['href'].rstrip('search/')+'/children/byname'
+                q={'name':building}
+                sleep(0.1)
+                r2=BeautifulSoup(requests.get('http://mosgorizbirkom.ru/'+a, params=q, timeout=0.5).text)
+                if r2.find('a')==None:
+                    result='null'
+                else:
+                    a2=r2.find('a')['href'].rstrip('search/').strip('/precinct/address/')
+                    sleep(0.1)
+                    r_final= BeautifulSoup(requests.get('http://mosgorizbirkom.ru/precinct/precinct/boundary/byaddress/'+a2, timeout=0.5).text)
+                    if r_final.find('p', class_='value')==None:
+                        result='null'
+                    else:
+                        result=r_final.find('p', class_='value').get_text()
+        except RequestException:
+            sleep(0.2)
+            result='null'
+            continue
+        if result=='null':
+            null_cnt+=1
+        all_cnt+=1
+        df_for_address_parse.at[idx, 'UIK']=result
+        df_for_address_parse.at[idx, 'parse_flg']=1
+        if idx%10==0:
+            print(all_cnt, null_cnt,time()-tnow)
+        if all_cnt%100==0:
+            df_for_address_parse.to_csv('df_for_address_parse.csv')
+    ```
+    Таким образом, получаем УИКи для пользователей Delivery
+    
+    ##### Теперь мы хотим узнать как эти люди голосовали 
+    
+    Для этого опять воспользуемся селениумом и спарсим
+    систему ГАС выборы с результатами голосвания на выборах в Госдуму в 2021 году
+    
+    1. Получаем список районов с ссылками на сводные таблицы с результатом
+    ```python
+    driver.get('http://www.moscow-city.vybory.izbirkom.ru/region/region/region/izbirkom?action=show&root=1000259&tvd=100100225883701&vrn=100100225883172&prver=0&pronetvd=null&region=77&sub_region=77&type=464&report_mode=null')
+    from selenium.webdriver.common.by import By
+    moscow=driver.find_elements(By.XPATH, "//*[@id='100100225883696']/ul/li/ul/li/a")
+    res_dict={}
+    for district in moscow:
+        if 'район' in district.text:
+            res_dict[district.text]=district.get_attribute('href')
+    ```
+    2. По этим ссылкам парсим таблицы 
+    
+    ```python 
+    res_dict_tables={}
+    for key, value in res_dict.items():
+        driver.get(value)
+        sleep(1)
+        table=pd.read_html(driver.page_source)[5]
+        table.to_csv(key+'.csv')
+        res_dict_tables[key]=table
+    ```
+    Вот и все теперь у нас есть данные о том как голосовали люди
     
     """
+
+
 
 
 
