9586d7d25d2dad565ffb27889cd7730e2e6e9220
olegbar02 <olegbar02@icloud.com>
2022-06-13 13:07:03 +0300

    4th

--- pages/Maps.py
+++ pages/Maps.py
@@ -3,13 +3,32 @@ import folium
 import requests
 import streamlit as st
 import pandas as pd
-from Data_loading_and_analysis import get_data, init_connection
+
 """# Здесь мы будем рисвать карты"""
 
 st.set_page_config(layout='wide')
 
 with st.echo(code_location='below'):
+    @st.experimental_singleton
+    def init_connection():
+        return psycopg2.connect(**st.secrets["postgres"])
 
+
+    conn = init_connection()
+
+
+    # Perform query.
+    # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+    @st.experimental_memo(ttl=600)
+    def run_query(query):
+        with conn.cursor() as cur:
+            cur.execute(query)
+            return cur.fetchall()
+
+
+    @st.experimental_singleton()
+    def get_data(query):
+        return pd.read_sql(query, conn)
     """Получим данные"""
     conn = init_connection()
     delivery_data = get_data("""select delivery2_user_id as user_id
